b56695c92bd35fc08cf67ddba3783134c38738a2
juanlramos <juanl.bnr@gmail.com>
2025-04-19 15:28:10 -0300

    feat: footer criado closes #5

--- src/shared/contexts/LayoutContext.tsx
+++ src/shared/contexts/LayoutContext.tsx
@@ -1,7 +1,10 @@
 import { createContext, useContext } from "react";
-import { Layout } from "antd";
+import { Button, Layout, Typography } from "antd";
+import { FaInstagram, FaFacebook, FaXTwitter } from "react-icons/fa6";
 
 const { Header, Content, Footer } = Layout;
+
+const Link = Typography.Link;
 
 interface ILayoutContext {}
 
@@ -14,10 +17,54 @@ interface ILayoutProvider {
 export const LayoutProvider = ({ children }: ILayoutProvider) => {
   return (
     <LayoutContext.Provider value={{}}>
-      <Layout style={{minHeight: "100vh"}}>
+      <Layout style={{ minHeight: "100vh" }}>
         <Header>Header</Header>
         <Content>{children}</Content>
-        <Footer>Footer</Footer>
+        <Footer
+          style={{
+            textAlign: "center",
+          }}
+        >
+          <p>
+            <b>Candle Shop</b>
+          </p>
+          <p>
+            <Button
+              type="text"
+              icon={<FaInstagram />}
+              size="large"
+              shape="circle"
+              target="_blank"
+              href="https://www.instagram.com/velas_candle_shop/"
+            />
+            <Button
+              type="text"
+              icon={<FaFacebook />}
+              size="large"
+              shape="circle"
+              target="_blank"
+              href="https://www.facebook.com/velas.candle.shop"
+            />
+            <Button
+              type="text"
+              icon={<FaXTwitter />}
+              size="large"
+              shape="circle"
+              target="_blank"
+              href="https://twitter.com/"
+            />
+          </p>
+          <p>
+            &copy; 2025
+            {new Date().getFullYear() == 2025 ? "" : -new Date().getFullYear()}
+          </p>
+          <p>
+            Website Criated by{" "}
+            <Link href="https://github.com/juanlramos" target="_blank">
+              Juan Lucas
+            </Link>
+          </p>
+        </Footer>
       </Layout>
     </LayoutContext.Provider>
   );
--- src/shared/themes/LightTheme.tsx
+++ src/shared/themes/LightTheme.tsx
@@ -1,11 +1,15 @@
 export const LightTheme = {
     token: {
-
+        colorPrimary: "#FF5733",
+        colorLink: "#FF5733",
     },
     components: {
         Layout: {
             headerBg: "#EEEEEE",
             footerBg: "#EEEEEE",
         },
+        Typography: {
+            linkHoverDecoration: "underline",
+        }
     }
 };
